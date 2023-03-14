60698437df8e662598941a6c6ab0b812deaf1b74
Martin Graham <martin.graham.web@gmail.com>
2023-03-14 20:17:45 +0000

    added income component added

---
+++ src/components/IncomeInputs.jsx
@@ -0,0 +1,28 @@
+import React, {useState} from 'react';
+
+function IncomeInputs() {
+    const [mainIncome, setmainIncome] = useState('');
+    const [additionalIncome, setadditionalIncome] = useState('');
+
+    const handleMainChange = (event) => {
+        setmainIncome(event.target.value);
+    };
+    const handleAdditionalChange = (event) => {
+        setadditionalIncome(event.target.value);
+    };
+
+return (
+    <div>
+        <label>
+            Main Income:
+            <input type="number" value={mainIncome} onChange={handleMainChange} />
+        </label>
+        <br />
+        <label>
+            <input type="number" value={additionalIncome} onChange={handleAdditionalChange} />
+        </label>
+    </div>
+);
+}
+
+export default IncomeInputs;
