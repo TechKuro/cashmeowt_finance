acb406687cd2e9ec2c6da17072aaeff48ec87c87
iona <ionachadwick97@gmail.com>
2023-03-16 21:02:07 +0000

    started working on the ticker

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,8 +1,27 @@
-import {Menu, Image} from "semantic-ui-react"
+import {Menu, Image} from "semantic-ui-react";
+import Marquee from "react-fast-marquee";
+import React, { useState, useEffect } from 'react';
+
 
 
 const Nav = () => {
+
+    const [stockData, setStockData] = useState(0);
+
+    useEffect(() => {
+        const apiKey = "ebe3699433b327a341b2e8a0d2c8d11d"
+        const api = "https://financialmodelingprep.com/api/v3/profile/AAPL,GOOG,MSFT,FB,AMZN,NVDA,TSLA,NFLX?apikey=" + apiKey;
+        
+        
+        fetch(api).then(res => res.json()).then((data) => {
+            setStockData(data)
+           
+           console.log(stockData)
+        })
+    },[]);
+
     return (
+        <div>
         <Menu secondary>
             <Image src="./images/cashLogo.png" size="small" />
 
@@ -10,6 +29,14 @@ const Nav = () => {
             <Menu.Item >About us</Menu.Item>
             <Menu.Item position="right">Login</Menu.Item>
         </Menu>
+        <Marquee>
+  {stockData ? stockData.map((item) => {
+    return (
+    <Ticker item={item}/>
+    )
+  }): ""}
+</Marquee>
+        </div>
     );
 };
 
