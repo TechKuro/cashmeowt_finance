9622aafb3a9f7e69bc821832e5c96f82dca55b29
iona <ionachadwick97@gmail.com>
2023-03-16 18:52:12 +0000

    added companies to the call, and written the code for the specific information we want. Will work on a loop to make the code neater

--- src/temporary.js
+++ src/temporary.js
@@ -1,5 +1,5 @@
 const apiKey = "ebe3699433b327a341b2e8a0d2c8d11d"
-const api = "https://financialmodelingprep.com/api/v3/profile/AAPL?apikey=" + apiKey;
+const api = "https://financialmodelingprep.com/api/v3/profile/AAPL,GOOG,MSFT,FB,AMZN,NVDA,TSLA,NFLX?apikey=" + apiKey;
 
 
-fetch(api).then(res => res.json()).then((data) => {console.log(data)})
+fetch(api).then(res => res.json()).then((data) => {console.log(data[0].symbol, data[0].price, data[0].changes, data[1].symbol, data[1].price, data[1].changes, data[2].symbol, data[2].price, data[2].changes, data[3].symbol, data[3].price, data[3].changes, data[4].symbol, data[4].price, data[4].changes, data[5].symbol, data[5].price, data[5].changes, data[6].symbol, data[6].price, data[6].changes, data[7].symbol, data[7].price, data[7].changes)})
