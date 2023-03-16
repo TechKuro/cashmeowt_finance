e7fd4ab5c239c20ab1e6e398cfaa9af2243addb6
Martin Graham <martin.graham.web@gmail.com>
2023-03-16 20:23:38 +0000

    added Navbar logo

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,14 @@
-import {Menu} from "semantic-ui-react"
+import {Menu, Image} from "semantic-ui-react"
+
 
 const Nav = () => {
     return (
-        <Menu>
-            <Menu.Item>Reset Screen</Menu.Item>
-            <Menu.Item>Login</Menu.Item>
-            <Menu.Item>About Us</Menu.Item>
+        <Menu secondary>
+            <Image src="./images/cashLogo.png" size="small" />
+
+            <Menu.Item position="right">Reset Screen</Menu.Item>
+            <Menu.Item >About us</Menu.Item>
+            <Menu.Item position="right">Login</Menu.Item>
         </Menu>
     );
 };
