ca5e21c77cb665ed2bb09c247641ce2fbc3358e2
Martin Graham <martin.graham.web@gmail.com>
2023-03-21 20:47:25 +0000

    added stuff

--- src/App.js
+++ src/App.js
@@ -43,6 +43,7 @@ function App() {
             additionalIncome={additionalIncome}
             setadditionalIncome={setadditionalIncome}
             mainIncome={mainIncome} />      
+            
          </div>
       </div>    
   )
