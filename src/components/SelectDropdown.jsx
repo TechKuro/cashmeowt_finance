d411d6644e2ff8d71a3b739a428165e41514a776
gideonclottey <gideonclotteyint@gmail.com>
2023-03-21 20:40:01 +0000

    selectdrop down feature fixed

--- src/components/SelectDropdown.jsx
+++ src/components/SelectDropdown.jsx
@@ -1,13 +1,13 @@
-const SelectDropdown = ({options, onChange}) => {
-    return(
-        <select onChange={onChange}>
-        {options.map((option, index) => (
-           <option key={index} value={option.value}>
-            {option.label}
-           </option> 
-        ))}
-        </select>
-    )
-}
+const SelectDropdown = ({ options, setSelectionOptions }) => {
+  return (
+    <select onChange={(e) => setSelectionOptions(e.target.value)}>
+      {options.map((option, index) => (
+        <option key={index} value={option.value}>
+          {option.label}
+        </option>
+      ))}
+    </select>
+  );
+};
 
 export default SelectDropdown;
