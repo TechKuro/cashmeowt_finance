c694a5ecb37289582790224056c9d6571c00bb75
Lukas <lukinko22@yahoo.co.uk>
2023-03-23 13:51:59 +0000

    tick hidden mobile view

--- src/components/InputList.jsx
+++ src/components/InputList.jsx
@@ -34,7 +34,7 @@ const InputList = ({ onAdd, totalExpenses, setTotalExpenses }) => {
           <div key={index} style={{ display: "flex", alignItems: "center" }}>
             <input type="number" value={item.value} readOnly className='block w-[80%] rounded-md border-0 py-1.5 pl-7 pr-10 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="0.00' />
             <span className="ml-3"></span>
-            <span
+            <span className="invisible md:visible"
               style={{
                 color: "green",
                 fontSize: "1.5em",
